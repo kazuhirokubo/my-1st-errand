72e59943e374c39cefa3c69c164bc480f6827191
kubox <kubox@328w.co.jp>
2016-10-11 10:38:01 +0900

    モデルクラスにGetter, Setterのみ追記 コンストラクタに関して、lombok適用すると、build時にエラーとなるため直書き

--- app/src/main/java/com/example/kazu/myapplication/ListViewItemModel.java
+++ app/src/main/java/com/example/kazu/myapplication/ListViewItemModel.java
@@ -1,21 +1,23 @@
 package com.example.kazu.myapplication;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
 /**
  * Created by kubox on 2016/10/07.
  */
 
+
+
 public class ListViewItemModel {
+
+    @Getter
+    @Setter
     private String date;
 
-    public void setDate(String date){
-        this.date = date;
-    }
-
-    public String getDate(){
-        return this.date;
-    }
-
     public ListViewItemModel(String date){
-        this.setDate(date);
+        this.date = date;
     }
 }
